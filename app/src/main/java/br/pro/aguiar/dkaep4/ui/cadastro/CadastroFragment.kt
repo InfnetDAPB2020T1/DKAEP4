242f1523b5fd1a09d40d36fa8d67bef544b449d7
Thiago Vieira de Aguiar <thiago.vieiradeaguiar@gmail.com>
2020-05-15 11:07:55 -0300

    Forumlário de Cadastro de Filme: Função storeFilme.

--- app/src/main/java/br/pro/aguiar/dkaep4/ui/cadastro/CadastroFragment.kt
+++ app/src/main/java/br/pro/aguiar/dkaep4/ui/cadastro/CadastroFragment.kt
@@ -1,6 +1,7 @@
 package br.pro.aguiar.dkaep4.ui.cadastro
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import br.pro.aguiar.dkaep4.R
+import kotlinx.android.synthetic.main.fragment_cadastro.*
 
 class CadastroFragment : Fragment() {
 
@@ -22,10 +24,26 @@ class CadastroFragment : Fragment() {
         cadastroViewModel =
             ViewModelProviders.of(this).get(CadastroViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_cadastro, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        cadastroViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnCadastroFilmeCadastrar.setOnClickListener {
+            cadastroViewModel.storeFilme(
+                edtTxtCadastroFilmeNome,
+                edtTxtCadastroFilmeAno,
+                edtTxtCadastroFilmeCategoria
+            )
+
+            Log.d("EditText", edtTxtCadastroFilmeNome.text.toString())
+            Log.d("EditText", edtTxtCadastroFilmeAno.text.toString())
+            Log.d("EditText", edtTxtCadastroFilmeCategoria.text.toString())
+
+            Log.d("EditText", edtTxtCadastroFilmeNome.text.isBlank().toString())
+            Log.d("EditText", edtTxtCadastroFilmeNome.text.isEmpty().toString())
+        }
     }
 }
--- app/src/main/java/br/pro/aguiar/dkaep4/ui/cadastro/CadastroViewModel.kt
+++ app/src/main/java/br/pro/aguiar/dkaep4/ui/cadastro/CadastroViewModel.kt
@@ -1,13 +1,45 @@
 package br.pro.aguiar.dkaep4.ui.cadastro
 
+import android.util.Log
+import android.widget.EditText
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.pro.aguiar.dkaep4.model.Filme
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CadastroViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    init {
+        Log.d("EditText", "CadastroViewModel iniciado")
     }
-    val text: LiveData<String> = _text
+
+    fun storeFilme(
+        edtTxtNome: EditText,
+        edtTxtAno: EditText,
+        edtTxtCategoria: EditText
+    ): Task<DocumentReference> {
+       if (
+           edtTxtNome.text.isNullOrBlank() ||
+           edtTxtAno.text.isNullOrBlank() ||
+           edtTxtCategoria.text.isNullOrBlank()
+       )
+           throw object : Throwable() {
+               override val message: String?
+                   get() = "Preencha todos os campos."
+           }
+
+       val firebaseFirestore = FirebaseFirestore.getInstance()
+       val collection = firebaseFirestore.collection("filme")
+       val task = collection.add(
+           Filme(
+               edtTxtNome.text.toString(),
+               edtTxtAno.text.toString().toInt(),
+               edtTxtCategoria.text.toString()
+           )
+       )
+       return task
+    }
 }
