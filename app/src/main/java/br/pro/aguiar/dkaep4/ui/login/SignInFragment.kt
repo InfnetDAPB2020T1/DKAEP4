6c66477d7456bff55d9255b758127a2a1e048b93
Thiago Vieira de Aguiar <thiago.vieiradeaguiar@gmail.com>
2020-05-22 11:52:34 -0300

    SignInFragment: AuthViewModel.signInUser: Listeners

--- app/src/main/java/br/pro/aguiar/dkaep4/ui/login/SignInFragment.kt
+++ app/src/main/java/br/pro/aguiar/dkaep4/ui/login/SignInFragment.kt
@@ -1,5 +1,6 @@
 package br.pro.aguiar.dkaep4.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import br.pro.aguiar.dkaep4.HomeActivity
 
 import br.pro.aguiar.dkaep4.R
 import kotlinx.android.synthetic.main.fragment_sign_in.*
@@ -36,15 +39,38 @@ class SignInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        try {
-            authViewModel.signInUser(
-                edtTxtLoginEmail, edtTxtLoginSenha
-            )
-        } catch (e: Throwable){
-            Toast.makeText(
-                this.context, e.message,
-                Toast.LENGTH_LONG
-            ).show()
+        btnLoginAcessar.setOnClickListener {
+            try {
+                authViewModel.signInUser(
+                    edtTxtLoginEmail, edtTxtLoginSenha
+                )
+                    .addOnSuccessListener {
+                        if (it != null){
+                            activity?.let {
+                                startActivity(Intent(it, HomeActivity::class.java))
+                            }
+                        } else {
+                            Toast.makeText(
+                                this.context, "Algo de errado ocorreu!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }.addOnFailureListener {
+                        Toast.makeText(
+                            this.context, it.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+            } catch (e: Throwable){
+                Toast.makeText(
+                    this.context, e.message,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        btnLoginCadstro.setOnClickListener {
+            findNavController().navigate(R.id.signUp_destination)
         }
 
     }
