8505f92dbc6d8e9f41e089bcc334b7b9cefaad47
Thiago Vieira de Aguiar <thiago.vieiradeaguiar@gmail.com>
2020-05-22 11:00:32 -0300

    SignUp: AuthViewModel: Instancia da ViewModel

--- app/src/main/java/br/pro/aguiar/dkaep4/ui/login/SignUpFragment.kt
+++ app/src/main/java/br/pro/aguiar/dkaep4/ui/login/SignUpFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 
 import br.pro.aguiar.dkaep4.R
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class SignUpFragment : Fragment() {
+
+    lateinit var authViewModel: AuthViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +26,29 @@ class SignUpFragment : Fragment() {
             R.layout.fragment_sign_up,
             container, false)
     }
+
+    // Instanciar a ViewModel
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity?.let {
+            authViewModel = ViewModelProviders.of(it)
+                .get(AuthViewModel::class.java)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnCadastroCadastrar.setOnClickListener {
+            authViewModel.createUser(
+                edtTxtCadastroEmail,
+                edtTxtCadastroSenha,
+                edtTxtCadastroRepetirSenha
+            )
+        }
+
+    }
+
+
 
 }
