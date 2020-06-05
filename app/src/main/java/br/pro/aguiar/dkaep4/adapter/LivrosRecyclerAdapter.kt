6d6894da11a20625566da1aae34d4b9fca0941c1
Thiago Vieira de Aguiar <thiago.vieiradeaguiar@gmail.com>
2020-06-05 11:33:18 -0300

    03/06/2020 Retrofit: ListaLivroRecyclerAdapter.

--- app/src/main/java/br/pro/aguiar/dkaep4/adapter/LivrosRecyclerAdapter.kt
+++ app/src/main/java/br/pro/aguiar/dkaep4/adapter/LivrosRecyclerAdapter.kt
@@ -3,9 +3,11 @@ package br.pro.aguiar.dkaep4.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.pro.aguiar.dkaep4.R
 import br.pro.aguiar.dkaep4.model.Livro
+import kotlinx.android.synthetic.main.layout_livros_item.view.*
 
 class LivrosRecyclerAdapter(
         var livros: MutableList<Livro>
@@ -15,7 +17,8 @@ class LivrosRecyclerAdapter(
 
     class LivrosViewHolder(view: View)
         : RecyclerView.ViewHolder(view)  {
-
+        val txtVwTitulo: TextView = view.txtVwLivroTitulo
+        val txtVwAno: TextView = view.txtVwLivroAno
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LivrosViewHolder {
@@ -31,10 +34,10 @@ class LivrosRecyclerAdapter(
 
         return livrosViewHolder
     }
-
     override fun getItemCount(): Int = livros.size
-
     override fun onBindViewHolder(holder: LivrosViewHolder, position: Int) {
-
+        val livro = livros[position]
+        holder.txtVwTitulo.text = livro.titulo
+        holder.txtVwAno.text = livro.ano.toString()
     }
 }
