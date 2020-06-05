fc65f8e5a797b0a9f4576ed8af4db1c4afee1fcf
Thiago Vieira de Aguiar <thiago.vieiradeaguiar@gmail.com>
2020-06-05 10:54:20 -0300

    03/06/2020 Retrofit: HomeLivrosActivity: Layout_lista_livros com RecyclerView

--- app/src/main/java/br/pro/aguiar/dkaep4/ui/ListaLivros/ListaLivrosFragment.kt
+++ app/src/main/java/br/pro/aguiar/dkaep4/ui/ListaLivros/ListaLivrosFragment.kt
@@ -1,4 +1,4 @@
-package br.pro.aguiar.dkaep4.ui.home
+package br.pro.aguiar.dkaep4.ui.ListaLivros
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import br.pro.aguiar.dkaep4.R
 
-class HomeFragment : Fragment() {
+class ListaLivrosFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var listaLivrosViewModel: ListaLivrosViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        listaLivrosViewModel =
+            ViewModelProviders.of(this).get(ListaLivrosViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_lista_livros, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        listaLivrosViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/br/pro/aguiar/dkaep4/ui/ListaLivros/ListaLivrosViewModel.kt
+++ app/src/main/java/br/pro/aguiar/dkaep4/ui/ListaLivros/ListaLivrosViewModel.kt
@@ -1,10 +1,10 @@
-package br.pro.aguiar.dkaep4.ui.home
+package br.pro.aguiar.dkaep4.ui.ListaLivros
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class ListaLivrosViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
